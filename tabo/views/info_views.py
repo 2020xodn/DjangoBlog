391d51cea36ce8f72ec1c9c4737e5d3b8715613d
2020xodn <64435609+2020xodn@users.noreply.github.com>
2021-06-13 05:42:28 +0900

    Posting 4*4 Layout

--- tabo/views/info_views.py
+++ tabo/views/info_views.py
@@ -7,7 +7,7 @@ def show_info(request):
         ["학과", "컴공"],
         ["학번", "17~"],
         ["분야", "Java, Unity"],
-        ["메일", "bbpasd@gmail.com"],
+        ["메일", "B L I N D !"],
         ["깃허브", "https://github.com/2020xodn"],
         ["작성일", "21-06-06"],
     ]
